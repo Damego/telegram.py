e661134e851a541f09c3a78647c037b1441f1880
Damego <danyabatueff@gmail.com>
2023-02-18 11:40:00 +0500

    refactor: mark data as optional in `BaseRequest.request`

--- telegram/models/protocols.py
+++ telegram/models/protocols.py
@@ -4,5 +4,5 @@ from ..http.route import Route
 
 
 class BaseRequest(Protocol):
-    async def request(self, route: Route, data: dict):
+    async def request(self, route: Route, data: dict | None = None) -> dict:
         raise NotImplementedError()
